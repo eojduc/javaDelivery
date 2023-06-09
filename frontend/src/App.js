ebffa951a66a31a6be87ec9746a38557372830c4
Delali Cudjoe <edc39@duke.edu>
2023-06-09 01:13:11 -0700

    minor edits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,17 @@ import {useState, useEffect} from "react";
 const App = () => {
   console.log('App');
   const [projects, setProjects] = useState([]);
-  useEffect(() => axios.get("localhost:8080/projects").then(response => {
-    setProjects(response.data)
-  }), []);
+  useEffect(() => {
+    axios.get("http://localhost:8080/projects").then(response => {
+      setProjects(response.data);
+    });
+  }, []);
   return (
     <div className="App">
       {projects.map(project => <iframe key={project.id}
           src={`https://calendar.google.com/calendar/embed?src=${project.calendarId}&ctz=America%2FLos_Angeles`} width="800" height="600"></iframe>)
       }
-      <h1>Hello</h1>
+      <h1>Goodbye</h1>
     </div>
   );
 }
