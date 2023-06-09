84a050131f51426347acfa1fbeca09422bfc0924
Delali Cudjoe <edc39@duke.edu>
2023-06-09 02:13:47 -0700

    added timezone support to projects, updated frontend to match

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
 import axios from "axios";
-import {useState, useEffect} from "react";
+import {React, useState, useEffect} from "react";
 const App = () => {
-  console.log('App');
   const [projects, setProjects] = useState([]);
+  const [selectedProject, setSelectedProject] = useState(null);
+
   useEffect(() => {
     axios.get("http://localhost:8080/projects").then(response => {
       setProjects(response.data);
     });
   }, []);
+  const projectSelectHandler = (event) => {
+    projects.filter(project => project.id === event.target.value)
+      .forEach(project =>
+          setSelectedProject(project))
+  }
   return (
-    <div className="App">
-      {projects.map(project => <iframe key={project.id}
-          src={`https://calendar.google.com/calendar/embed?src=${project.calendarId}&ctz=America%2FLos_Angeles`} width="800" height="600"></iframe>)
-      }
-      <h1>Goodbye</h1>
+    <div className="App" >
+      <select onChange={projectSelectHandler}>
+        <option value={null}>Select a project</option>
+        {projects.map(project => <option key={project.id} value={project.id}>{project.name}</option>)}
+      </select>
+      {selectedProject && <iframe title={selectedProject.name} key={selectedProject.id}
+                                  src={`https://calendar.google.com/calendar/embed?src=${selectedProject.calendarId}`} width="800" height="600"></iframe>}
     </div>
   );
 }
