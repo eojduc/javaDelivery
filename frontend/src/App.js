3a4676f1cd10b4f78dd6dad035f45034557fa204
Delali Cudjoe <edc39@duke.edu>
2023-06-09 02:21:39 -0700

    rebuilt frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {React, useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 const App = () => {
   const [projects, setProjects] = useState([]);
   const [selectedProject, setSelectedProject] = useState(null);
